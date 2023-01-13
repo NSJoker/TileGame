d2b57270e9d38367827e2908374fefabaa090908
NSJoker <laughofnsjoker@gmail.com>
2023-01-13 16:54:39 +0530

    Initial Commit

---
+++ TileGame/TileGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TileGameApp.swift
+//  TileGame
+//
+//  Created by Chandrachudh K on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TileGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
